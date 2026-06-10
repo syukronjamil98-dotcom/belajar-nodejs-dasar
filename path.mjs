b9c7df85d5a0cd21a54177d08add3618b1890486
Mohamad Syukron Jamil <syukron.jamil98@gmail.com>
2026-06-10 12:26:09 +0700

    add path.mjs

---
+++ path.mjs
@@ -0,0 +1,7 @@
+import path from "path";
+
+const file = "/Users/PLN/Desktop/Frontend/belajar-nodejs-dasar";
+
+console.log(path.dirname(file));
+console.log(path.basename(file));
+console.log(path.extname(file));
