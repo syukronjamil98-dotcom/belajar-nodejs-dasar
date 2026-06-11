4d78076fd38fed1526a2a90ad02f6cd485287142
Mohamad Syukron Jamil <syukron.jamil98@gmail.com>
2026-06-11 10:42:18 +0700

    add process.mjs and readline.mjs

---
+++ process.mjs
@@ -0,0 +1,17 @@
+import process from "process";
+
+process.addListener("exit", (exitCode) =>{
+    console.log(`NodeJS exit with code ${exitCode}`);
+
+})
+
+console.log(process.version);
+console.table(process.argv);
+console.table(process.report);
+console.table(process.env);
+
+process.exit(1);
+
+console.log("Hello");
+
+
