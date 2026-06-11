c542486afb0c32904439d82c82fa1d509cfacd08
Mohamad Syukron Jamil <syukron.jamil98@gmail.com>
2026-06-11 09:50:45 +0700

    Menambah Event

--- file-system/makeFile.mjs
+++ file-system/makeFile.mjs
@@ -1,5 +1,10 @@
-import fs from 'fs/promises';
+//Level 1
+// import fs from 'fs/promises';
+// await fs.writeFile('file.txt', 'Hello, World!')
+// console.log('File created successfully!');
 
-await fs.writeFile('file.txt', 'Hello, World!')
-
-console.log('File created successfully!');
+//Level 2
+// import fs from "fs/promises";
+// const nama = "Syukron";
+// await fs.writeFile("file.txt", `Halo ${nama}`);
+// console.log("Sudah dibuat");
---
+++ globals.mjs
@@ -0,0 +1,4 @@
+setTimeout(function() {
+    console.log("Hello Globals");
+}, 1000);
+
